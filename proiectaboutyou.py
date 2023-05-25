dd3771b4d99c3ffbe8f07c44763de58f2e3f2f38
BiancaHartaganu <bianca.hartaganu@yahoo.com>
2023-05-25 22:15:03 +0300

    second commit

--- proiectaboutyou.py
+++ proiectaboutyou.py
@@ -1,5 +1,7 @@
 import unittest
 from time import sleep
+
+from selenium.common import NoSuchElementException
 from selenium.webdriver.common.keys import Keys
 
 from selenium import webdriver
@@ -24,22 +26,19 @@ class Login(unittest.TestCase):
         self.chrome.implicitly_wait(3)
         sleep(2)
         self.chrome.find_element(By.CSS_SELECTOR, "#onetrust-accept-btn-handler").click()
-        element = WebDriverWait(self.chrome, 3).until(EC.visibility_of_element_located(self.FORM_AUTHENTICATION))
-        element.click()
-        element2 = WebDriverWait(self.chrome, 3).until(EC.visibility_of_element_located(self.FORM_AUTHENTICATION2))
-        element2.click()
+        # element = WebDriverWait(self.chrome, 3).until(EC.visibility_of_element_located(self.FORM_AUTHENTICATION))
+        # element.click()
+        # element2 = WebDriverWait(self.chrome, 3).until(EC.visibility_of_element_located(self.FORM_AUTHENTICATION2))
+        #element2.click()
 
     def setUpWithoutLoginModal(self):
         self.chrome = webdriver.Chrome(ChromeDriverManager().install())
         self.chrome.maximize_window()
         self.chrome.get("https://www.aboutyou.ro")
 
-    def tearDown(self):
-        self.chrome.quit()
-
     def test_1_verificare_url(self):
         actual_url = self.chrome.current_url
-        expected_url = "https://www.aboutyou.ro/magazinul-tau?loginFlow=login"
+        expected_url = "https://www.aboutyou.ro/magazinul-tau"
         assert actual_url == expected_url
 
     def test_2_verificare_titlu(self):
@@ -48,36 +47,54 @@ class Login(unittest.TestCase):
         assert actual_title == expected_title
 
     def test_3_verificare_text(self):
+        element = WebDriverWait(self.chrome, 3).until(EC.visibility_of_element_located(self.FORM_AUTHENTICATION))
+        element.click()
+        element2 = WebDriverWait(self.chrome, 3).until(EC.visibility_of_element_located(self.FORM_AUTHENTICATION2))
+        element2.click()
         h1_element = self.chrome.find_element(By.CSS_SELECTOR, '.jRNzAJ > span:nth-child(1) > span:nth-child(2)')
         h1_text = h1_element.text
         assert h1_text == "Apple"
-
-
     def test_4_afisare_buton_login(self):#aici am testat daca butonul de log-in este afisat
-        butonul_login = self.chrome.find_element(By.CSS_SELECTOR, "div.sc-3ffazj-1:nth-child(2)")
+        element = WebDriverWait(self.chrome, 3).until(EC.visibility_of_element_located(self.FORM_AUTHENTICATION))
+        element.click()
+        element2 = WebDriverWait(self.chrome, 3).until(EC.visibility_of_element_located(self.FORM_AUTHENTICATION2))
+        element2.click()
+        butonul_login = self.chrome.find_element(By.CSS_SELECTOR, "div.sc-3ffazj-1:nth-child(2)") #cauta elementul login
         self.assertTrue(butonul_login.is_displayed())
     def test_5_verificare_href_link(self):
+        element = WebDriverWait(self.chrome, 3).until(EC.visibility_of_element_located(self.FORM_AUTHENTICATION))
+        element.click()
+        element2 = WebDriverWait(self.chrome, 3).until(EC.visibility_of_element_located(self.FORM_AUTHENTICATION2))
+        element2.click()
         self.chrome.find_element(By.CSS_SELECTOR, "div.sc-3ffazj-1:nth-child(1)").click()
         self.chrome.implicitly_wait(10)
         expected_url = "https://www.aboutyou.ro/?loginFlow=login"
         assert self.chrome.current_url != expected_url
 
-    def test_6_verificare_date_logare_eroare(self): #am verificat daca apare mesajul la introducerea gresita a datelor de login
+    def test_6_verificare_capcha(self): #am verificat daca apare mesajul la introducerea gresita a datelor de login
         sleep(1)
-        self.chrome.find_element(By.CSS_SELECTOR, "div.sc-4wvktd-3:nth-child(1) > label:nth-child(1)").send_keys("aaa@yahoo.com")
+        self.chrome.find_element(By.CLASS_NAME, "li167jd").click()
+        sleep(2)
+        WebDriverWait(self.chrome, 3).until(EC.visibility_of_element_located(self.FORM_AUTHENTICATION2)).click()
         sleep(1)
-        self.chrome.find_element(By.CSS_SELECTOR, "div.sc-4wvktd-3:nth-child(2) > label:nth-child(1)").send_keys("aaa")
+        self.chrome.find_element(By.CSS_SELECTOR, "div.sc-4wvktd-3:nth-child(1) > label:nth-child(1)").send_keys("aaa@yahoo.com")#am introdus adresa de mail gresita
         sleep(1)
-        self.chrome.find_element(By.XPATH, "//span[@class='sc-iprg3j-1 jPkEJg'][normalize-space()='Logare']").click()
+        self.chrome.find_element(By.CSS_SELECTOR, "div.sc-4wvktd-3:nth-child(2) > label:nth-child(1)").send_keys("aaa")#am introdus parola gresita
         sleep(1)
-        #error_elem = self.chrome.find_element(By.XPATH, "//div[@class='sc-ovefx6-6 gCMsSs']")
-        expected_error = "Te rugăm să-ți verifici datele."
-        sleep(1)
-        self.assertEqual(expected_error, "Mesajul nu este cel asteptat")
-        #assert error_elem.text == expected_error
+        self.chrome.find_element(By.XPATH, "//span[@class='sc-iprg3j-1 jPkEJg'][normalize-space()='Logare']").click()#am dat click pe login
+        sleep(2)
+        try:
+            self.chrome.find_element(By.XPATH,'//span[contains(text(),"e-mail") and @class="sc-qywjzy-1 kCVWSN"]')
+            assert False, "Logarea a fost efectuata, desi nu am bifat capcha"
+        except NoSuchElementException:
+            print("Logarea nu a putut fi efectuata, fara bifarea capcha")
 
 
     def test_7_verificare_buton_x(self): #am verificat daca se poate inchide fereastra de login la apasarea botunului x
+        element = WebDriverWait(self.chrome, 3).until(EC.visibility_of_element_located(self.FORM_AUTHENTICATION))
+        element.click()
+        element2 = WebDriverWait(self.chrome, 3).until(EC.visibility_of_element_located(self.FORM_AUTHENTICATION2))
+        element2.click()
         self.chrome.find_element(By.CSS_SELECTOR, "svg.sc-1ahb4we-0:nth-child(2)").click()
         sleep(1)
         try:
@@ -87,37 +104,30 @@ class Login(unittest.TestCase):
 
     def test_8_login_cu_succes(self): #am verificat daca la introducerea datelor corecte se poate loga pe pagina
         sleep(1)
+        element = WebDriverWait(self.chrome, 3).until(EC.visibility_of_element_located(self.FORM_AUTHENTICATION))
+        element.click()
+        element2 = WebDriverWait(self.chrome, 3).until(EC.visibility_of_element_located(self.FORM_AUTHENTICATION2))
+        element2.click()
         self.chrome.find_element(By.CSS_SELECTOR, "div.sc-4wvktd-3:nth-child(1) > label:nth-child(1)").send_keys(
-            "biancatest@yahoo.com")
+            "biancatest@yahoo.com")#am introdus adresa de mail corecta
         self.chrome.find_element(By.CSS_SELECTOR, "div.sc-4wvktd-3:nth-child(2) > label:nth-child(1)").send_keys(
-            "parola")
+            "parola")#am introdus parola corecta
         self.chrome.find_element(By.CSS_SELECTOR, ".sc-ovefx6-15 > span:nth-child(1)").click()
         sleep(3)
-        name_elem = self.chrome.find_element(By.CSS_SELECTOR, "div.sth0sda:nth-child(1) > span:nth-child(2)")
-        expected = "bianca"
-        assert name_elem.text == expected
-
-    def test_9_adaugare_produs_cos(self): #aici am verificat daca se poate adauga un produs in cos
+    def test_9_cautare_produs_copii(self): #aici am verificat daca cauta un produs in categoria de copii a paginii
         sleep(1)
-        self.chrome.find_element(By.CSS_SELECTOR, "svg.sc-1ahb4we-0:nth-child(2)").click()
-        self.chrome.find_element(By.CSS_SELECTOR, "li.n12ggm7p:nth-child(3) > a:nth-child(1) > div:nth-child(1)").click()
-
-        self.chrome.find_element(By.CSS_SELECTOR, ".sj9vzi").send_keys("Sneaker low 'Mayze Stack Feelin Xtra Wns' negru")
-        self.chrome.find_element(By.CSS_SELECTOR, ".sj9vzi").send_keys(Keys.ENTER)
-        sleep(3)
-        self.chrome.find_element(By.CSS_SELECTOR, "#\\39 089723 > div:nth-child(1) > img:nth-child(1)").click()
+        self.chrome.find_element(By.XPATH, "//a[@class='sc-1x1l7vk-1 ichvGd'][normalize-space()='Copii']").click()
+        self.chrome.find_element(By.XPATH, "//*[@id='@aboutyou/router::SCROLL_ANCHOR']/div[2]/section/div[2]/section/div[1]/ul/li[1]/a/div[1]/img").click()
+        try:
+            self.chrome.find_element(By.XPATH, "//img[@alt='Tommy Hilfiger Underwear']")
+        except Exception as e:
+            print("Brand-ul produsului nu apare pe pagina")
         sleep(2)
-        self.chrome.find_element(By.CSS_SELECTOR, ".sc-3xrapk-2").click()
-        sleep(3)
-        self.chrome.find_element(By.CSS_SELECTOR, "li.sc-1pi0myn-1:nth-child(1) > div:nth-child(1) > label:nth-child(1) > div:nth-child(2)").click()
-        sleep(2)
-        self.chrome.find_element(By.CSS_SELECTOR, "button.kwtDkx:nth-child(1) > div:nth-child(2)").click()
-        sleep(6)
 
     def test_10_schimbare_tara_site(self):#aici am verificat daca se poate schimba tara de origine a site-ului
 
-        self.chrome.find_element(By.CSS_SELECTOR, "svg.sc-1ahb4we-0:nth-child(2)").click()
-        sleep(1)
+        # self.chrome.find_element(By.CSS_SELECTOR, "svg.sc-1ahb4we-0:nth-child(2)").click()
+        # sleep(1)
         self.chrome.find_element(By.CSS_SELECTOR, ".sc-sjmh1l-4").click()
         sleep(1)
         self.chrome.find_element(By.CSS_SELECTOR, ".sc-8ng12u-6").click()
@@ -125,6 +135,6 @@ class Login(unittest.TestCase):
         self.chrome.find_element(By.CSS_SELECTOR, "a.sc-1hnuqjs-3:nth-child(2)").click()
         sleep(2)
         text = self.chrome.find_element(By.CSS_SELECTOR, ".sc-sjmh1l-4").text
-        expected = "AT"
+        expected = "AT"#am schimbat tara din romania in austria
 
         assert expected == text
